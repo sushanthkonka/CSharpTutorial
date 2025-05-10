65892e46bf4a789decd098d160fbcea91303a467
Sushanth Konka <skcgn@umsystem.edu>
2025-05-10 12:48:16 -0500

    Boolean Program

---
+++ BooleanProgram/BooleanProgram.cs
@@ -0,0 +1,16 @@
+class BooleanProgram
+{
+    public static bool Boolean_type()
+    {
+        bool isCSharpFun = true;
+        return isCSharpFun;
+
+    }
+
+    public static bool Boolean_type2()
+    {
+        bool isFishTasty = false;
+        return isFishTasty;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine(MathProgram.Increment(4));
-        Console.WriteLine(C_StringProgram.String_Assign());
-        Console.WriteLine(C_StringProgram.String_Interpolation());
-        Console.WriteLine(C_StringProgram.String_Access());
-        Console.WriteLine(C_StringProgram.String_Text());
-    }
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine(MathProgram.Increment(4));
+        // Console.WriteLine(C_StringProgram.String_Assign());
+        // Console.WriteLine(C_StringProgram.String_Interpolation());
+        // Console.WriteLine(C_StringProgram.String_Access());
+        // Console.WriteLine(C_StringProgram.String_Text());
+
+        Console.WriteLine(BooleanProgram.Boolean_type());
+        Console.WriteLine(BooleanProgram.Boolean_type2());
+    } 
 }
 
