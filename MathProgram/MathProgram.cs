ec7d12124f93a3451b734b2a2210f1d0c2ae1c01
Sushanth Konka <skcgn@umsystem.edu>
2025-05-10 08:14:30 -0500

    CSharp Math Practice.

---
+++ MathProgram/MathProgram.cs
@@ -0,0 +1,8 @@
+internal static class MathProgram
+{
+    public  static int Increment(int number)
+    {
+        return number+1;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine(MathProgram.Increment(10));
     }
 }
